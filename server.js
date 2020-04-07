4ce8ffca39c89e140f682fac5ba28ec38ca93c03
mccor2000 <roccm0200@gmail.com>
2020-04-07 18:09:08 +0700

    Challenge 2 complete.

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 
 app.route("/").get((req, res) => {
-  res.render("pug/index");
+  res.render(process.cwd() + "/views/pug/index", {
+    title: "Hello",
+    message: "Please login",
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => {
