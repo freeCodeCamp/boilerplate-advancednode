8c321b3f267f830c49fe8d54b58eb04c417d36eb
mccor2000 <roccm0200@gmail.com>
2020-04-16 08:47:49 +0700

    Finish challenge 3

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
+const session = require("express-session");
+const passport = require("passport");
+
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -11,6 +14,16 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "pug");
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "roccm.mccor",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route("/").get((req, res) => {
   res.render(process.cwd() + "/views/pug/index", {
