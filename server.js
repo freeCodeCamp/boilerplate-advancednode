8d0c91dc511d6857591df8c9a4165b5520190190
Jeremy Fiel <jeremyrfiel@gmail.com>
2020-04-18 23:24:09 -0400

    update depds, remove unnecessary local files, update readme, package, server.  fixes {freeCodeCamp/freeCodeCamp/issuse/38404}

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-'use strict';
+"use strict";
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const express = require("express");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.route('/')
-  .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route("/").get((req, res) => {
+  //Write your code here to render the Pug template
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
