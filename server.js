2deee71b6010455c3700a52ac17ae1389488814d
Sky020 <2397860h@student.gla.ac.uk>
2020-06-12 16:57:24 +0100

    add: first lesson

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
 
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+  res.render(process.cwd() + './views/pug');
 });
 
 app.listen(process.env.PORT || 3000, () => {
