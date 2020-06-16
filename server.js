06d79b55458e7b3903157495df9e8614b8f9b38e
Sky020 <2397860h@student.gla.ac.uk>
2020-06-16 17:16:40 +0100

    add packages for hosting

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 "use strict";
 require('dotenv').config()
 const express = require("express");
+const myDB = require('./connection');
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
@@ -9,7 +10,6 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'pug');
 
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
