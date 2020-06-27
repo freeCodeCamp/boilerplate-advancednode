9172bf85a6106063921bc6e89170d2e4e68ba5f6
Sky020 <2397860h@student.gla.ac.uk>
2020-06-27 18:46:16 +0100

    add: socketio necessary files

---
+++ public/client.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+
+
+    // Form submittion with new message in field with id 'm'
+    $('form').submit(function () {
+        var messageToSend = $('#m').val();
+        //send message to server here?
+        $('#m').val('');
+        return false; // prevent form submit from refreshing page
+    });
+
+
+
+});
