3afc872e60daf2961f5ab10a19b006fb05b0ef84
Sky020 <2397860h@student.gla.ac.uk>
2020-10-07 21:51:22 +0100

    ensure quotations are single

--- public/client.js
+++ public/client.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   // Form submittion with new message in field with id 'm'
-  $("form").submit(function () {
-    var messageToSend = $("#m").val();
+  $('form').submit(function () {
+    var messageToSend = $('#m').val();
 
-    $("#m").val("");
+    $('#m').val('');
     return false; // prevent form submit from refreshing page
   });
 });
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-"use strict";
-require("dotenv").config();
-const express = require("express");
-const myDB = require("./connection");
-const fccTesting = require("./freeCodeCamp/fcctesting.js");
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use("/public", express.static(process.cwd() + "/public"));
+app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route("/").get((req, res) => {
-  res.render("Load your view here");
+app.route('/').get((req, res) => {
+  res.render('Load your view here');
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+  console.log('Listening on port ' + process.env.PORT);
 });
