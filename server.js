1df233bbdc181e388fcb8cb32cce21471d1b8551
James-Prentice <Jamesprenticez@gmail.com>
2021-01-09 22:17:40 +1300

    Use a Template Engine's Powers

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug')
 
 app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug')
+  res.render(process.cwd() + '/views/pug', {title: 'Hello', message: 'Please login'} )
 });
 
 app.listen(process.env.PORT || 3000, () => {
