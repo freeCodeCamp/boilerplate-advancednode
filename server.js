a7c60ac9258452c45821120e57a62827bb5b9d03
Desire Kaleba <desirekaleba@gmail.com>
2021-01-23 00:11:16 +0300

    fix Listening on port undefined (#16)
    * fix Listening on port undefined
    
    this fixes the message Listening on port undefined logged to console when the server is running but process.env.PORT is undefined
    
    * Update server.js
    
    add EOF
    
    Co-authored-by: Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
    
    Co-authored-by: Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.route('/').get((req, res) => {
   res.render('Load your view here');
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Listening on port ' + process.env.PORT);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
 });
