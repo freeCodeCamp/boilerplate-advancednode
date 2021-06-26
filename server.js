5e0dc2f8b516dacad283a5c5af262f7a4c91cf53
Erika Lira <35104052+erikalira@users.noreply.github.com>
2021-06-26 21:09:24 +0000

    feat: Serialization of a User Object

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 app.set('view engine', 'pug')
@@ -25,6 +26,16 @@ app.use(passport.session());
 
 app.route('/').get((req, res) => {
   res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
+});
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  // });
 });
 
 const PORT = process.env.PORT || 3000;
