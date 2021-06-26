7f635937796d48fdb33bd0f7bca8136de7ebef82
Erika Lira <35104052+erikalira@users.noreply.github.com>
2021-06-26 20:20:50 +0000

    feat: Set up a Template Engine

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'pug')
+
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render('pug');
 });
 
 const PORT = process.env.PORT || 3000;
