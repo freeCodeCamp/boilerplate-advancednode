415b9e8eedc30e505b02671d2bfd9ee12e21dc6b
Erika Lira <35104052+erikalira@users.noreply.github.com>
2021-06-26 20:26:52 +0000

    feat: Use a Template Engine's Powers

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug')
 
 app.route('/').get((req, res) => {
-  res.render('pug');
+  res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
 });
 
 const PORT = process.env.PORT || 3000;
