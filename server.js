2f1e14c8dbfbe462fd8bdff41f1bf140423c94e3
Replit user <>
2023-10-29 22:09:36 +0000

    pug setup

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
-
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
