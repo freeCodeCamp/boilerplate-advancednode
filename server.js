3f6423ac707b0f4cdaad3e5edb7c9822464be366
Replit user <>
2023-10-30 21:25:51 +0000

    setup passport

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const passport = require('passport');
+const session = require('express-session');
 
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 fccTesting(app); //For FCC testing purposes
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
