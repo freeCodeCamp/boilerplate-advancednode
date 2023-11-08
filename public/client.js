f991c44a364961d617545df444422df69594ea65
Replit user <>
2023-11-08 15:17:44 +0000

    adding chat

--- public/client.js
+++ public/client.js
@@ -1,8 +1,22 @@
-$(document).ready(function () {
-  // Form submittion with new message in field with id 'm'
-  $('form').submit(function () {
-    var messageToSend = $('#m').val();
+$(document).ready(function() {
+  /*global io*/
+  let socket = io();
 
+  socket.on('user', function(data) {
+    $('#num-users').text(data.currentUsers + ' users online');
+    let message =
+      data.username + (data.connected ? ' has joined the chat.' : ' has left the chat.');
+    $('#messages').append($('<li>').html('<b>' + message + '</b>'));
+  });
+
+  socket.on('chat message', (data) => {
+    $('#messages').append($('<li>').text(`${data.username}: ${data.message}`));
+  });
+
+  // Form submittion with new message in field with id 'm'
+  $('form').submit(function() {
+    var messageToSend = $('#m').val();
+    socket.emit('chat message', messageToSend);
     $('#m').val('');
     return false; // prevent form submit from refreshing page
   });
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 'use strict';
 require('dotenv').config();
+
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const passport = require('passport');
+
 const session = require('express-session');
+const passport = require('passport');
+const passportSocketIo = require('passport.socketio');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
 
 const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 fccTesting(app); //For FCC testing purposes
@@ -14,7 +28,9 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -22,11 +38,67 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' });
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
+myDB(async client => {
+  const myDataBase = await client.db('test').collection('users');
+  routes(app, myDataBase);
+  auth(app, myDataBase);
+  let currentUsers = 0;
+  io.on('connection', (socket) => {
+    ++currentUsers;
+    io.emit('user', {
+      user: socket.request.user,
+      username: socket.request.user.username,
+      currentUsers,
+      connected: true
+    });
+    console.log(`A user ${socket.request.user.username} has connected`);
+    socket.on('chat message', (data) => {
+      io.emit('chat message', { username: socket.request.user.username, message: data })
+    });
+
+
+    socket.on('disconnect', () => {
+      --currentUsers;
+      console.log('A user has disconnected');
+      io.emit('user', {
+        username: socket.request.user.username,
+        currentUsers,
+        connected: false
+      });
+    });
+
+  });
+
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database', showLogin: true });
+  });
 });
 
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
