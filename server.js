2cd809a7e67d6b351196df1fb221aa0fec1167e9
carmen lm <carmen.info.lm@gmail.com>
2024-05-28 19:55:31 +0000

    Main * feat: init gitpod config
    * add freecodecamp.dev to testing file
    
    Checklist:
    
    <!-- Please follow this checklist and put an x in each of the boxes, like this: [x]. It will ensure that our team takes your pull request seriously. -->
    
    - [ ] I have read [freeCodeCamp's contribution guidelines](https://contribute.freecodecamp.org).
    - [ ] My pull request has a [descriptive title](https://contribute.freecodecamp.org/#/how-to-open-a-pull-request?id=prepare-a-good-pr-title) (**not** a vague title like `Update index.md`)
    
    <!--If your pull request closes a GitHub issue, replace the XXXXX below with the issue number.-->
    
    Closes #XXXXX
    
    <!-- Feel free to add any additional description of changes below this line -->

--- server.js
+++ server.js
@@ -3,16 +3,30 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
+const res = require('express/lib/response.js');
+const session = require('express-session');
+const passport = require('passport');
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 
 app.route('/').get((req, res) => {
-
+  res.render('index', { title: 'Hello', message: 'Please log in' });
 });
 
 const PORT = process.env.PORT || 3000;
